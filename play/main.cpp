40652e12248ceda15e3203359381cae9367092e9
Chao Qu <chaoqu@rivian.com>
2025-08-22 21:38:20 +0000

    Try to decode

--- play/main.cpp
+++ play/main.cpp
@@ -1,5 +1,24 @@
+#include <algorithm>
+#include <filesystem>
 #include <fmt/core.h>
 #include <glog/logging.h>
+#include <string>
 
+#include "lidar_types.h"
 
-int main(int argc, char **argv) { return 0; }
+namespace fs = std::filesystem;
+namespace hl = hesai::lidar;
+
+int main(int argc, char **argv) {
+  const fs::path packets_dir = "/home/ubuntu/data/temp/hesai_packets";
+  const fs::path angle_correction_file = packets_dir / "QT128C2X_angle.csv";
+  const fs::path firetime_correction_file =
+      packets_dir / "QT128C2X_firetime.csv";
+
+  std::vector<fs::path> packets_files;
+  std::copy_if(fs::directory_iterator(packets_dir), fs::directory_iterator(),
+               std::back_inserter(packets_files),
+               [](const fs::path &p) { return p.extension() == ".bin"; });
+  std::sort(packets_files.begin(), packets_files.end());
+  LOG(INFO) << "Number of packets: " << packets_files.size();
+}
